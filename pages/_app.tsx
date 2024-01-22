d6247b7517795ba7a43be75fb57e0512e371e2ac
KenzoBenzo <32865577+KenzoBenzo@users.noreply.github.com>
2024-01-22 16:46:43 -0600

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { Layout } from "@/components/layout";
 import "@/styles/globals.css";
 import { Theme } from "@radix-ui/themes";
 import '@radix-ui/themes/styles.css';
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Theme accentColor="mint" grayColor="sage" radius="large" >
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </Theme>
   );
