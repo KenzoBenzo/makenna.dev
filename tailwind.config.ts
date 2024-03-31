c12f24dc4556208d3fd21429225fb1b1de8d02e3
KenzoBenzo <32865577+KenzoBenzo@users.noreply.github.com>
2024-03-31 17:02:24 -0400

    fix ordered list styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./utils/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
