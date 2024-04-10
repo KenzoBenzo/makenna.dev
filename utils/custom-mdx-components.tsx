897296dbb3d089cce49cb92fb2c81447bbe7bb74
KenzoBenzo <32865577+KenzoBenzo@users.noreply.github.com>
2024-04-10 18:50:28 -0400

    Formatting and new components

--- utils/custom-mdx-components.tsx
+++ utils/custom-mdx-components.tsx
@@ -1,36 +1,47 @@
-import { CohereArrowButton, CohereButton, CohereButtonProps } from "@/components/cohere-examples";
+import { CodeBlock } from "@/components/codeblock";
+import {
+  CohereArrowButton,
+  CohereButton,
+  CohereButtonProps,
+} from "@/components/cohere-examples";
 import { ExampleFrame, ExampleFrameProps } from "@/components/example-frame";
 import { StyledExternalLink } from "@/components/styled-external-link";
 import { Blockquote, Code } from "@radix-ui/themes";
 import clsx from "clsx";
+import Image, { ImageProps } from "next/image";
+import { CodeBlockProps } from "react-code-block/dist/code-block";
 
 export const mdxComponents = {
   h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h1 className="text-2xl mt-8 mb-3 text-sage-12">{props.children}</h1>
+    <h1 className='text-2xl mt-8 mb-3 text-sage-12'>{props.children}</h1>
   ),
   h2: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h2 className="text-xl mt-8 mb-3 text-sage-12">{props.children}</h2>
+    <h2 className='text-xl mt-8 mb-3 text-sage-12'>{props.children}</h2>
   ),
   h3: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h3 className="text-lg mt-8 mb-2 text-sage-11">{props.children}</h3>
+    <h3 className='text-lg mt-8 mb-2 text-sage-11'>{props.children}</h3>
   ),
   h4: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h4 className="text-md mt-8 mb-2 text-sage-11">{props.children}</h4>
+    <h4 className='text-md mt-8 mb-2 text-sage-11'>{props.children}</h4>
   ),
   p: (props: React.HTMLAttributes<HTMLParagraphElement>) => (
-    <p className="text-md mb-6">{props.children}</p>
+    <p className='text-md mb-6'>{props.children}</p>
   ),
   ul: (props: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className="list-['—_'] list-inside marker:text-mint-10 mt-2 space-y-1 mb-3">{props.children}</ul>
+    <ul className="list-['—_'] list-inside marker:text-mint-10 mt-2 space-y-1 mb-3">
+      {props.children}
+    </ul>
   ),
   ol: (props: React.HTMLAttributes<HTMLOListElement>) => (
-    <ol className="list-decimal list-inside marker:text-mint-11 mt-2 space-y-1 mb-3">{props.children}</ol>
+    <ol className='list-decimal list-inside marker:text-mint-11 mt-2 space-y-1 mb-3'>
+      {props.children}
+    </ol>
   ),
   li: (props: React.HTMLAttributes<HTMLLIElement>) => (
-    <li className="text-sage-11">{props.children}</li>
+    <li className='text-sage-11'>{props.children}</li>
   ),
   blockquote: (props: React.HTMLAttributes<HTMLQuoteElement>) => (
-    <Blockquote size="3" className="text-sage-10">
+    <Blockquote size='3' className='text-sage-10'>
       {props.children}
     </Blockquote>
   ),
@@ -43,9 +54,28 @@ export const mdxComponents = {
     <StyledExternalLink {...props}>{props.children}</StyledExternalLink>
   ),
   hr: (props: React.HtmlHTMLAttributes<HTMLHRElement>) => (
-    <hr className="border-sage-3 my-8" {...props} />
+    <hr className='border-sage-3 my-8' {...props} />
   ),
-  ExampleFrame: (props: ExampleFrameProps) => <ExampleFrame className={clsx("my-4", props.className)} {...props} />,
-  CohereArrowButton: (props: CohereButtonProps) => <CohereArrowButton {...props} />,
-  CohereButton: (props: CohereButtonProps) => <CohereButton {...props} />
-}
+  ExampleFrame: (props: ExampleFrameProps) => (
+    <ExampleFrame className={clsx("my-4", props.className)} {...props} />
+  ),
+  CohereArrowButton: (props: CohereButtonProps) => (
+    <CohereArrowButton {...props} />
+  ),
+  CohereButton: (props: CohereButtonProps) => <CohereButton {...props} />,
+  Image: ({
+    alt,
+    aspectRatio,
+    className,
+    ...props
+  }: ImageProps & { aspectRatio: string }) => (
+    <div
+      className={clsx(`aspect-[32/21] relative`, {
+        [`aspect-[${aspectRatio}]`]: aspectRatio !== undefined,
+      })}
+    >
+      <Image alt={alt ?? ""} className={clsx("rounded-xl mx-auto", className)} {...props} />
+    </div>
+  ),
+  CodeBlock: ({ ...props }: Omit<CodeBlockProps, "children">) => <CodeBlock {...props} />
+};
