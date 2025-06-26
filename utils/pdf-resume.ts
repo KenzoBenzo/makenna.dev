0bb699172c0dfabc5b1b62b8fe5f1320d08befb3
Makenna Smutz <32865577+KenzoBenzo@users.noreply.github.com>
2025-06-25 21:55:32 -0400

    Refine CV PDF layout

--- utils/pdf-resume.ts
+++ utils/pdf-resume.ts
@@ -1,23 +1,45 @@
 import { ExperiencesQuery } from './graphql-generated'
 
 export function generateResumePdf(experiences: ExperiencesQuery['experiences']): Buffer {
-  const header = '%PDF-1.4\n'
   const escape = (text: string) => text.replace(/[()\\]/g, '\\$&')
+  const header = '%PDF-1.4\n'
+
+  const title = 'Makenna Smutz'
+  const contact = 'https://makenna.dev'
+
   const lines = experiences.map(
-    (e) => `${e.jobTitle} @ ${e.company} (${e.dateRange})`
+    (e) => `- ${e.jobTitle} @ ${e.company} (${e.dateRange})`
   )
 
-  const contentLines = lines
-    .map((line) => `(${escape(line)}) Tj\nT* `)
-    .join('')
-  const contentStream = `BT\n/F1 12 Tf\n50 780 Td\n${contentLines}ET`
+  const lineHeight = 18
+  const startY = 720
+
+  const contentParts = [
+    'BT',
+    '/F1 20 Tf',
+    '50 780 Td',
+    `(${escape(title)}) Tj`,
+    '/F1 12 Tf',
+    `0 -24 Td`,
+    `(${escape(contact)}) Tj`,
+    `50 ${startY} Td`,
+    `${lineHeight} TL`
+  ]
+
+  lines.forEach((line, idx) => {
+    if (idx > 0) contentParts.push('T*')
+    contentParts.push(`(${escape(line)}) Tj`)
+  })
+  contentParts.push('ET')
+
+  const contentStream = contentParts.join('\n')
 
   const objects = [
     '1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n',
     '2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n',
     '3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n',
     `4 0 obj\n<< /Length ${contentStream.length} >>\nstream\n${contentStream}\nendstream\nendobj\n`,
-    '5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n',
+    '5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n'
   ]
 
   let offset = header.length
